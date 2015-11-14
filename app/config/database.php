55a05acbd78b76f3a2be27e8d66d80857a25b4e9
root <ratulcse10@gmail.com>
2015-11-14 04:37:09 -0500

    server side commit for getting pictures in the public/uploads folder

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'rps',
+			'username'  => 'root',
+			'password'  => 'SUSTcse',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
